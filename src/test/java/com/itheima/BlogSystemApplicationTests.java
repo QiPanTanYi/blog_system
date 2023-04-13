caf75529f22f821cfc059ceeab38d10711f5d5b5
谭谭 <100819988+QiPanTanYi@users.noreply.github.com>
2023-04-14 00:11:28 +0800

    Add files via upload

---
+++ src/test/java/com/itheima/BlogSystemApplicationTests.java
@@ -0,0 +1,21 @@
+package com.itheima;
+
+import org.junit.jupiter.api.Test;
+
+//import org.testng.annotations.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
