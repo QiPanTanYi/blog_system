9e008ec0c488304047b12e1e2607169ab725f23a
谭谭 <100819988+QiPanTanYi@users.noreply.github.com>
2023-05-25 11:19:53 +0800

    Update BlogSystemApplicationTests.java

--- src/test/java/com/itheima/BlogSystemApplicationTests.java
+++ src/test/java/com/itheima/BlogSystemApplicationTests.java
@@ -1,21 +1,25 @@
 package com.itheima;
 
-import org.junit.jupiter.api.Test;
-
-//import org.testng.annotations.Test;
+import com.itheima.web.scheduletask.ScheduleTask;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogSystemApplicationTests {
-
-    @Test
     public void contextLoads() {
     }
-
+    @Autowired
+    private ScheduleTask scheduleTask;
+    @Test
+    public void sendSimpleMailTest() {
+        scheduleTask.sendEmail();
+        System.out.println("邮件发送成功！");
+    }
+    // 这里只是测试了邮箱，如果要进行邮箱测试的话，必须把ScheduleTask类的第28行注释取消，然后注释掉第27行
 }
